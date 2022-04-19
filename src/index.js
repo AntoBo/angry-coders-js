e020042fd2e6cadf29f54db802538d317e1ab50a
AntoBo <anton.bondarenkooooo@gmail.com>
2022-04-19 17:35:16 +0300

    pag init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/styles.scss';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
+import * as TUI from './js/pagination';
+
+//TUI pagination for markup and styles
+const pagination = new Pagination('pagination', TUI.getOptions(500));
