e7cf22ac80366ccf15a93f4c02e38f7c3bd3907e
AnthonyUPTN <tagirovantn@gmail.com>
2022-04-21 18:47:38 +0300

    add genres

--- src/index.js
+++ src/index.js
@@ -9,9 +9,21 @@ function onLoadPage(page) {
   Promise.all([fetchAPI.fetchTrendingMovies(page), fetchAPI.fetchGenres()])
     .then(data => {
       console.log(data[0].data.results);
+      console.log(data[0].data.results[0].genre_ids);
+
       data[0].data.results.forEach(el => {
         el.release_date = el.release_date.slice(0, 4);
+
+        el.genre_ids = el.genre_ids.map(elem => {
+          data[1].data.genres.filter(el => {
+            if (el.id === elem) {
+              elem = el.name;
+            }
+          });
+          return elem;
+        });
       });
+
       document.querySelector('.gallery__container').innerHTML = templateCard(data[0].data.results);
       //modal works!
       new Modal(data[0].data.results);
