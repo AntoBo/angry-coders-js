616b91a37f78696084170aab1b44bbe646e6b749
AntoBo <anton.bondarenkooooo@gmail.com>
2022-04-22 21:19:35 +0300

    done

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import templateModalMarkup from '../templates/modalMarkup';
+import Markup from './markup';
 import LocalStorageAPI from './library';
 
 export default class Modal {
@@ -25,14 +25,16 @@ export default class Modal {
     if (event.target.nodeName === 'UL') {
       return;
     }
+    //take controls
     this.backdrop.classList.toggle('is-hidden');
     document.body.classList.add('no-scroll');
     window.addEventListener('keydown', this.closeModalByEsc.bind(this));
-    //draw modal card
+    //find card to draw modal
     const movieObj = this.moviesArray.find(
       option => option.id === Number(event.target.parentNode.dataset.id)
     );
-    document.querySelector('.js-modal').innerHTML = templateModalMarkup(movieObj);
+    //draw modal card
+    Markup.drawModal(movieObj);
 
     //take controls of buttons
     const toWatchedList = document.querySelector('[data-watched]');
@@ -63,47 +65,4 @@ export default class Modal {
       this.modalClose();
     }
   }
-
-  // addToList(listName, movieObj) {
-  //   const watchedList = JSON.parse(localStorage.getItem(this.WATCHED));
-  //   const queueList = JSON.parse(localStorage.getItem(this.QUEUE));
-  //   if (listName === this.WATCHED) {
-  //     if (watchedList.find(el => el.id === movieObj.id)) {
-  //       console.log('this movie already exists');
-  //       return;
-  //     }
-
-  //     //add item
-  //     watchedList.push(movieObj);
-
-  //     //remove item
-  //     if (queueList.find(el => el.id === movieObj.id)) {
-  //       queueList.splice(queueList.indexOf(movieObj), 1);
-  //     }
-
-  //     console.log('toWatchedList');
-  //     localStorage.setItem(this.WATCHED, JSON.stringify(watchedList));
-  //     localStorage.setItem(this.QUEUE, JSON.stringify(queueList));
-  //     return;
-  //   }
-
-  //   if (listName === this.QUEUE) {
-  //     if (queueList.find(el => el.id === movieObj.id)) {
-  //       console.log('this movie already exists');
-  //       return;
-  //     }
-
-  //     //add item
-  //     queueList.push(movieObj);
-
-  //     //remove item
-  //     if (watchedList.find(el => el.id === movieObj.id)) {
-  //       watchedList.splice(watchedList.indexOf(movieObj), 1);
-  //     }
-  //     console.log('toQueueList');
-  //     localStorage.setItem(this.WATCHED, JSON.stringify(watchedList));
-  //     localStorage.setItem(this.QUEUE, JSON.stringify(queueList));
-  //     return;
-  //   }
-  // }
 }
