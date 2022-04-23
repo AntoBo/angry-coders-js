20fd9257b403c0eb92df7e8e25d4b6dd0ca2b13c
AnthonyUPTN <tagirovantn@gmail.com>
2022-04-23 14:55:08 +0300

    done 80%

--- src/js/library.js
+++ src/js/library.js
@@ -3,44 +3,42 @@ export default class LocalStorageAPI {
   static QUEUE = 'queueList';
   constructor() {}
   static addToList(listName, movieObj) {
+    const toWatchedList = document.querySelector('[data-watched]');
+    const toQueueList = document.querySelector('[data-queue]');
     const watchedList = JSON.parse(localStorage.getItem(this.WATCHED));
     const queueList = JSON.parse(localStorage.getItem(this.QUEUE));
+
     if (listName === this.WATCHED) {
+      // remove item
       if (watchedList.find(el => el.id === movieObj.id)) {
-        console.log('this movie already exists');
+        watchedList.splice(watchedList.indexOf(movieObj), 1);
+        localStorage.setItem(this.WATCHED, JSON.stringify(watchedList));
+        toWatchedList.textContent = 'add to watched';
         return;
       }
 
       //add item
       watchedList.push(movieObj);
-
-      //remove item
-      if (queueList.find(el => el.id === movieObj.id)) {
-        queueList.splice(queueList.indexOf(movieObj), 1);
-      }
-
-      console.log('toWatchedList');
       localStorage.setItem(this.WATCHED, JSON.stringify(watchedList));
-      localStorage.setItem(this.QUEUE, JSON.stringify(queueList));
+      toWatchedList.textContent = 'remove watched';
+
       return;
     }
 
     if (listName === this.QUEUE) {
+      // remove item
       if (queueList.find(el => el.id === movieObj.id)) {
-        console.log('this movie already exists');
+        queueList.splice(queueList.indexOf(movieObj), 1);
+        localStorage.setItem(this.QUEUE, JSON.stringify(queueList));
+        toQueueList.textContent = 'add to form queue';
         return;
       }
 
       //add item
       queueList.push(movieObj);
-
-      //remove item
-      if (watchedList.find(el => el.id === movieObj.id)) {
-        watchedList.splice(watchedList.indexOf(movieObj), 1);
-      }
-      console.log('toQueueList');
-      localStorage.setItem(this.WATCHED, JSON.stringify(watchedList));
       localStorage.setItem(this.QUEUE, JSON.stringify(queueList));
+      toQueueList.textContent = 'remove form queue';
+
       return;
     }
   }
